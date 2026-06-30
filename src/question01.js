8feac61d1b00dcf18a0a7fe92777103eb4cada99
KASSIANE LUIZA DIAS <kassiane_dias@estudante.sesisenai.org.br>
2026-06-29 21:39:04 -0300

    atv 02

--- src/question01.js
+++ src/question01.js
@@ -6,3 +6,5 @@ var num2 = leia.questionInt("Digite um numero de 1 a 10: ")
 var num3 = leia.questionInt("Digite um numero de 1 a 10: ")
 var A;
 
+if(num1 > num2)
+    num1 = A
--- src/question02.js
+++ src/question02.js
@@ -1,11 +1,11 @@
 import leia from 'readline-sync'
 console.log("===Notas do carnaval 2025===")
-var nota1 = 5
-var nota2 = 1
-var nota3 = 9
-var nota4 = 9
-var nota5 = 8.5
-var nota6 = 10
+var nota1 = leia.questionFloat("Nota: ")
+var nota2 = leia.questionFloat("Nota: ") // menor nota
+var nota3 = leia.questionFloat("Nota: ")
+var nota4 = leia.questionFloat("Nota: ")
+var nota5 = leia.questionFloat("Nota: ")
+var nota6 = leia.questionFloat("Nota: ") //maior nota
 
 console.log("===Anuladas: segunda nota e sexta nota===")
 
@@ -15,4 +15,4 @@ if(media >= 7){
     console.log("Aprovado")
 } else {
     console.log("Reprovado")
-}
+} 
