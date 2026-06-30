c1a4ee71d7a84418063f3cdfaffbaf736562b652
KASSIANE LUIZA DIAS <kassiane_dias@estudante.sesisenai.org.br>
2026-06-29 21:40:54 -0300

    atv 02

--- src/question02.js
+++ src/question02.js
@@ -1,15 +1,15 @@
 import leia from 'readline-sync'
 console.log("===Notas do carnaval 2025===")
-var nota1 = leia.questionFloat("Nota: ")
-var nota2 = leia.questionFloat("Nota: ") // menor nota
+var nota1 = leia.questionFloat("Nota: ") // menor nota
+var nota2 = leia.questionFloat("Nota: ") 
 var nota3 = leia.questionFloat("Nota: ")
 var nota4 = leia.questionFloat("Nota: ")
 var nota5 = leia.questionFloat("Nota: ")
 var nota6 = leia.questionFloat("Nota: ") //maior nota
 
-console.log("===Anuladas: segunda nota e sexta nota===")
+console.log("===Anuladas: primeira nota e sexta nota===")
 
-var media =  (nota1+ nota3 + nota4 + nota5) / 4
+var media =  (nota2 + nota3 + nota4 + nota5) / 4
 
 if(media >= 7){
     console.log("Aprovado")
